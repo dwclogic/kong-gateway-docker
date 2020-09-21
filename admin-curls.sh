9a75d0e60d446dc2b7c414f50b60911cae2aa154
David Caldwell <dwclogic@gmail.com>
2020-09-21 10:32:58 -0600

    Added Admin curls from the Gateway via Bastion SSH connection

---
+++ admin-curls.sh
@@ -0,0 +1,5 @@
+# Commands run on the box via ssh
+
+curl -X GET --url http://localhost:8001/services/api-manager-service/routes | python -mjson.tool
+
+
--- curl-commands.sh
+++ curl-commands.sh
@@ -17,7 +17,7 @@ curl -i -X POST \
 #
 #{"host":"localhost","id":"ad6d3add-d1c5-477b-8533-a70d1ae7bdcd","protocol":"http","read_timeout":60000,"tls_verify_depth":null,"port":8001,"updated_at":1599853021,"ca_certificates":null,"created_at":1599853021,"connect_timeout":60000,"write_timeout":60000,"name":"api-manager-service","retries":5,"path":"\/","tls_verify":null,"client_certificate":null,"tags":null}%
 
-# Api Host Route
+# Api Host Route # not run on the AWS box.
 curl -i -X POST \
   --url http://localhost:8001/services/api-manager-service/routes \
   --data 'hosts[]=api-manager.org'
@@ -36,19 +36,44 @@ curl -i -X POST \
 # ------------------------------------------ # CONFIG ADMIN  ----------------------------------
 # First setup - using the path option, but the paths get long and confusing.
 # Securing the Admin API
-curl -X POST http://localhost:8001/services \
-  --data name=admin-api \
-  --data url=http://localhost:8001/
+#curl -X POST http://localhost:8001/services \
+#  --data name=admin-api \
+#  --data url=http://localhost:8001/
 # Response
 # {"host":"localhost","id":"09e6a9cc-d869-465c-9e35-926ba00b2c20","protocol":"http","read_timeout":60000,"tls_verify_depth":null,"port":8001,"updated_at":1599599038,"ca_certificates":null,"created_at":1599599038,"connect_timeout":60000,"write_timeout":60000,"name":"admin-api","retries":5,"path":"\/","tls_verify":null,"client_certificate":null,"tags":null}
 
 curl -i -X POST \
-  --url http://localhost:8001/services/admin-api/routes \
-  --data 'paths[]=/admin-api/'
+  --url http://localhost:8001/services/api-manager-service/routes \
+  --data 'paths[]=/api-manager-service/'
+#RESPONSE AWS
+# {
+#   "id":"57e691bc-f7b1-4527-b872-8d343d28cc31",
+#   "path_handling":"v0",
+#   "paths":["\/api-manager-service\/"],
+#   "destinations":null,
+#   "headers":null,
+#   "protocols":["http","https"],
+#   "created_at":1600470181,
+#   "snis":null,
+#   "service":{"id":"1fec427f-13af-41cf-86c7-414ce2594a86"},
+#   "name":null,
+#   "strip_path":true,
+#   "preserve_host":false,
+#   "regex_priority":0,
+#   "updated_at":1600470181,
+#   "sources":null,
+#   "methods":null,
+#   "https_redirect_status_code":426,
+#   "hosts":null,
+#   "tags":null
+# }
+
+curl -i -X GET \
+  --url http://localhost:8001/services/api-manager-service/routes
 
 curl -i -X POST http://localhost:8001/services/admin-api/routes \
   -H "Content-Type: application/json" \
-  -d '{"name": "admin-route", "paths": [ "/admin-api" ]}'
+  -d '{"name": "admin-route", "paths": [ "/api-manager-service" ]}'
 # Response
 # {"id":"d774d05d-afde-42d8-b131-d7a36eb16031",
 # "path_handling":"v0",
@@ -73,13 +98,38 @@ curl -i -X POST http://localhost:8001/services/admin-api/routes \
 
 # enabling Key-auth Plugin. for Api-admin-service
 curl -i -X POST \
-  --url http://localhost:8001/services/admin-api/plugins/ \
+  --url http://localhost:8001/services/api-manager-service/plugins/ \
   --data 'name=key-auth'
 # Response
 # {"created_at":1599860409,"consumer":null,"id":"0f406fc0-d55d-4f0e-a22b-200c11d08801","service":{"id":"09e6a9cc-d869-465c-9e35-926ba00b2c20"},"enabled":true,"route":null,"tags":null,"config":{"key_in_body":false,"key_names":["apikey"],"anonymous":null,"hide_credentials":false,"run_on_preflight":true},"protocols":["grpc","grpcs","http","https"],"name":"key-auth"}%
+# AWS # {"created_at":1600465843,"consumer":null,"id":"06dbadf9-bfc5-489f-a38a-92eff3f2b902","service":{"id":"1fec427f-13af-41cf-86c7-414ce2594a86"},"enabled":true,"route":null,"tags":null,"config":{"key_in_body":false,"key_names":["apikey"],"anonymous":null,"hide_credentials":false,"run_on_preflight":true},"protocols":["grpc","grpcs","http","https"],"name":"key-auth"}
 
 
+# make consumer for Admin service:
+# ------------------------------------------ # Consumer   ----------------------------------
+
+## Create a Consumer....
+curl -i -X POST \
+  --url http://localhost:8001/consumers/ \
+  --data "username=dwclogic"
+# Response
+# {
+#     "custom_id":null,
+#     "created_at":1599236454,
+#     "id":"34e7edd3-413b-4404-9c6d-3ccb517fa8c4",
+#     "tags":null,
+#     "username":"dwclogic"
+# }
+# AWS # {"custom_id":null,"created_at":1600465986,"id":"add0d442-f3e6-42e5-ae64-a88680ec7fce","tags":null,"username":"dwclogic"}
+
+#Add a key
+curl -i -X POST \
+  --url http://localhost:8001/consumers/dwclogic/key-auth/ \
+  --data 'key=06104FC3-0A62-4759-8606-8F3011441022' # real key is in my persomal notes file until we can get a better system setup::
 # ------------------------------------------ # CONFIG ADMIN  ----------------------------------
+
+
+
 
 # Direct to 8001 ! -------------------------------------
 # EXAMPLE ------------------------------------------ SERVICE
@@ -136,6 +186,12 @@ curl -i -X POST \
 #     "tags":null
 # }
 
+# AWS --------  make a rout version instead
+curl -i -X POST \
+  --url http://localhost:8001/services/example-service/routes \
+  --data 'paths[]=/example-service/'
+
+
 # Forward your requests trough Kong
 curl -i -X GET \
   --url http://localhost:8000/ \
@@ -165,22 +221,9 @@ curl -i -X POST \
 
 
 
-# ------------------------------------------ # Consumer   ----------------------------------
 # ---------------------------------------------------  CONSUMER calls
-## Create a Consumer....
-curl -i -X POST \
-  --url http://localhost:8001/consumers/ \
-  --data "username=David-Caldwell"
-# Response
-# {
-#     "custom_id":null,
-#     "created_at":1599236454,
-#     "id":"34e7edd3-413b-4404-9c6d-3ccb517fa8c4",
-#     "tags":null,
-#     "username":"David-Caldwell"
-# }
 
-## Provision a key credential for that consumer.
+## Provision a key credential for that consumer.  KEY IS OLD  WONT WORK NOW
 curl -i -X POST \
   --url http://localhost:8001/consumers/David-Caldwell/key-auth/ \
   --data 'key=06104FC3-0A62-4759-8606-8F3011441022'
